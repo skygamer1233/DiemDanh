2f6bd4fbe321c5eb6172e2d69586508f97abfb77
skygamer1233 <123954037+skygamer1233@users.noreply.github.com>
2024-06-17 21:18:20 +0700

    Update TabComplete.java

--- src/main/java/me/diemdanh/TabComplete.java
+++ src/main/java/me/diemdanh/TabComplete.java
@@ -22,6 +22,9 @@ public class TabComplete implements TabCompleter {
                 if (sender.hasPermission("diemdanh.giveticket")) {
                     completions.add("giveticket");
                 }
+                if (sender.hasPermission("diemdanh.top")) {
+                    completions.add("top");
+                }
             } else if (args.length == 2 && args[0].equalsIgnoreCase("giveticket")) {
                 for (Player player : Bukkit.getOnlinePlayers()) {
                     completions.add(player.getName());
